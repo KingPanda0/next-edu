682d2f40d5f33678ef0aa885f470fb979c86f5be
Belen Curcio <curciobelen@gmail.com>
2020-08-07 15:48:58 -0300

    changes

--- components/common/Search.js
+++ components/common/Search.js
@@ -20,13 +20,13 @@ export default function Search() {
             placeholder="Search Term"
           />
         </form>
-        <nav class="flex border-blue-dark items-center">
+        <nav class="flex flex-col md:flex-wrap md:flex-row border-blue-dark items-center justify-center">
           <div class="px-3 py-2 text-base text-sm text-center font-semibold tracking-wide uppercase">
             Encyclopedia Categories
           </div>
           <a
             href="#"
-            class="px-3 py-2 font-medium text-sm leading-5 rounded-md text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-100"
+            class="ml-4 px-3 py-2 font-medium text-sm leading-5 rounded-md text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 focus:bg-gray-100"
           >
             Health & Medicine
           </a>
