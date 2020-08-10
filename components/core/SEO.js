e385e5bd564b7f038327f19b19a7c1eb660029a1
Belen Curcio <curciobelen@gmail.com>
2020-08-10 16:57:26 -0300

    Adding good SEO Support'

--- components/core/SEO.js
+++ components/core/SEO.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { NextSeo } from "next-seo";
+import { NextSeo, ArticleJsonLd } from "next-seo";
 import siteConfig from "../../siteConfig.json";
 
 export default function SEO({
@@ -7,13 +7,34 @@ export default function SEO({
   title,
   description,
   pageTitle = siteConfig.title,
+  publishedAt = new Date(),
 }) {
+  const date = new Date(publishedAt).toISOString();
   return (
     <>
       <NextSeo
         title={title ? `${title} - ${pageTitle}` : pageTitle}
         canonical={url}
         description={description}
+        openGraph={{
+          url,
+          title,
+          description,
+          type: "article",
+          article: {
+            publishedTime: date,
+          },
+        }}
+      />
+      <ArticleJsonLd
+        authorName={siteConfig.author}
+        dateModified={date}
+        datePublished={date}
+        description={description}
+        publisherLogo="/image.png"
+        publisherName={siteConfig.author}
+        title={title}
+        url={url}
       />
     </>
   );
